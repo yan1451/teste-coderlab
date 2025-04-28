abe67a5486e63586f79660a92e47805adf5e406d
yan1451 <yan.brasileiro@gmail.com>
2025-04-27 21:30:22 -0300

    feat: add filter, update create and update functions to accept category IDs
    - Added a name filter to the `getAll` method to allow filtering products by name.
    - Updated `create` function to accept category IDs and associate them with the product.
    - Updated `update` function to accept category IDs for both associating and dissociating categories from the product.

--- backend/src/modules/product/product.service.ts
+++ backend/src/modules/product/product.service.ts
@@ -1,19 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/shared';
 import { Product } from '@prisma/client';
-import { NotFoundError } from 'src/shared';
+import { NotFoundError, ConflictException } from 'src/shared';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getAll(): Promise<Product[]> {
-    return this.prismaService.product.findMany();
+  async getAll(params?: any): Promise<Product[]> {
+    let name: string = '';
+    let page: number = 1;
+    let pageSize: number = 10;
+    if (params['name']) {
+      name = params['name'];
+    }
+    if (params['page']) {
+      page = parseInt(params['page']);
+    }
+    if (params['pageSize']) {
+      pageSize = parseInt(params['pageSize']);
+    }
+
+    return this.prismaService.product.findMany({
+      where: name
+        ? { name: { contains: name, mode: 'insensitive' } }
+        : undefined,
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+      include: {
+        categories: true,
+      },
+    });
   }
 
   async getById(id: string): Promise<Product | null> {
     const product = await this.prismaService.product.findUnique({
       where: { id },
+      include: {
+        categories: true,
+      },
     });
 
     if (!product) {
@@ -23,16 +50,55 @@ export class ProductService {
     return product;
   }
 
-  async create(data: Product): Promise<Product> {
+  async create(data: CreateProductDto): Promise<Product> {
+    const { categories, ...productData } = data;
+    const existingProduct = await this.prismaService.product.findFirst({
+      where: { name: data.name },
+    });
+
+    if (existingProduct) {
+      throw new ConflictException(data.name);
+    }
+
     return this.prismaService.product.create({
-      data,
+      data: {
+        ...productData,
+        categories: {
+          connect: Array.isArray(categories)
+            ? categories.map((categoryId: string) => ({ id: categoryId }))
+            : [],
+        },
+      },
     });
   }
 
-  async update(id: string, data: Partial<Product>): Promise<Product> {
+  async update(id: string, data: UpdateProductDto): Promise<Product> {
+    const { categories, ...productData } = data;
+
+    const existingProduct = await this.prismaService.product.findUnique({
+      where: { id },
+      include: { categories: true },
+    });
+
+    if (!existingProduct) {
+      throw new NotFoundError('Product');
+    }
+
     return this.prismaService.product.update({
       where: { id },
-      data,
+      data: {
+        ...productData,
+        categories: {
+          connect: Array.isArray(categories)
+            ? categories.map((categoryId: string) => ({ id: categoryId }))
+            : [],
+          disconnect: Array.isArray(categories)
+            ? existingProduct.categories
+                .filter((category) => !categories.includes(category.id))
+                .map((category) => ({ id: category.id }))
+            : [],
+        },
+      },
     });
   }
 
